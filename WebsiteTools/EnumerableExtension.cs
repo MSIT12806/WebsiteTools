558357215d1b2998ff9590e03b199361d4526c5a
robin <alegalone@gmail.com>
2024-12-18 09:17:51 +0800

    擴充 Enumerable

---
+++ WebsiteTools/EnumerableExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WebsiteTools
+{
+    public static class EnumerableExtension
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> src)
+        {
+            return src == null || src.Count() == 0;
+        }
+    }
+}
