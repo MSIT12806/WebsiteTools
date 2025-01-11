01bf39ebac12cda987208ae1753261221c8e7b12
robin <alegalone@gmail.com>
2025-01-11 21:44:09 +0800

    共用 view model

---
+++ WebsiteTools/ViewModels/ResponseViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebsiteTools.ViewModels
+{
+    public class ResponseViewModel
+    {
+        public string Message { get; set; }
+        public string Type { get; set; }
+        public object Data { get; set; }
+    }
+}
